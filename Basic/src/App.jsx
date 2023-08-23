da2ab442a0408b2a8b069af8625e87be74e300e2
Degner3 <niklaschristiansen@live.dk>
2023-08-23 11:26:13 +0200

    S

--- Basic/src/App.jsx
+++ Basic/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
+// UseState
 import { Intro } from './components/useState/Intro/Intro'
 import { Content } from './components/useState/Content/Content'
 import { HilsenState } from './components/useState/HilsenState/Hilsen'
 // import { Regner } from './components/useState/Regner/Regner'
 import { Taeller } from './components/useState/Taeller/Taeller'
+
+// UseEffect
 import { IntroEffect } from './components/useEffect/Intro/IntroEffect'
 import { Timer } from './components/useEffect/Timer/Timer'
+import { Validation } from './components/useEffect/Validation/Validation'
+import { ShowUsers } from './components/useEffect/ShowUsers/ShowUsers'
 
 
 
@@ -20,7 +25,10 @@ function App() {
       {/* <Regner/> */}
       {/* <IntroEffect/> */}
 
-      <Timer/>
+
+      {/* <Timer/> */}
+      {/* <Validation/> */}
+      {/* <ShowUsers/> */}
     </>
   )
 }
